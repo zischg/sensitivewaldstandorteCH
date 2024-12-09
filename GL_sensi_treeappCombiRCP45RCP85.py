3da7af04036816f6d456c425157e47380b247f01
Andreas Paul Zischg <andreas.zischg@giub.unibe.ch>
2024-12-09 22:29:43 +0100

    GL

--- GL_sensi_treeappCombiRCP45RCP85.py
+++ GL_sensi_treeappCombiRCP45RCP85.py
@@ -21,6 +21,8 @@ baumartenbedeutungenrcp45=gpd.read_file(projectspace+"/GL"+"/GL_rcp45_baumartenb
 baumartenbedeutungenrcp85=gpd.read_file(projectspace+"/GL"+"/GL_rcp85_baumartenbedeutungen.gpkg", layer="GL_rcp85_baumartenbedeutungen", driver="GPKG")
 combi=gpd.overlay(baumartenbedeutungenrcp45, baumartenbedeutungenrcp85, how='intersection', make_valid=True, keep_geom_type=True)
 
+arvenundlaerchen=['59','59A','59C','59E','59J','59L','59S','59V','59H','59R','72,' '59*','59G','59AG','59EG','59VG','72G','57CLä','57VLä','58Lä', '59Lä', '59ELä', '59LLä', '59VLä','59LLä']
+
 for item in combi.columns.tolist():
     if "FI" in item:
         print(item)
@@ -89,19 +91,22 @@ for col in treetypeslist:
         if row['ue_1'] == 0 and row[col + 'heu1_1'] not in ['a', 'b', 'c'] and row[col + 'zuk1_1'] not in ['a', 'b', 'c'] and row[ col + 'zuk1_2'] in ['c']:
             combi.loc[index, col] = 5
             combi.loc[index, col + "Fall"] = 14
-        #Achtung 6
-        if col == "GO" and row['ue_1'] == 0 and row[col + 'heu1_1'] in ['c'] and row[col + 'zuk1_1'] in ['c'] and row[ col + 'zuk1_2'] not in ['a','b','c']:
-            combi.loc[index, col] = 6
-            combi.loc[index, col + "Fall"] = 18
-        if col == "GO" and row['ue_1'] == 0 and row[col + 'heu1_1'] in ['c'] and row[col + 'zuk1_1'] not in ['a','b','c'] and row[ col + 'zuk1_2']  in ['c']:
-            combi.loc[index, col] = 6
-            combi.loc[index, col + "Fall"] = 19
-        if col == "GO" and row['ue_1'] == 0 and row[col + 'heu1_1'] not in ['a','b','c'] and row[col + 'zuk1_1'] in ['c'] and row[ col + 'zuk1_2'] not in ['a','b','c']:
-            combi.loc[index, col] = 6
-            combi.loc[index, col + "Fall"] = 20
-        if col == "GO" and row['ue_1'] == 0 and row[col + 'heu1_1'] not in ['a','b','c'] and row[col + 'zuk1_1'] not in ['a','b','c'] and row[ col + 'zuk1_2'] in ['c']:
-            combi.loc[index, col] = 6
-            combi.loc[index, col + "Fall"] = 21
+        ##Achtung 7
+        #if col == "GO" and row['ue_1'] == 0 and row[col + 'heu1_1'] in ['c'] and row[col + 'zuk1_1'] in ['c'] and row[ col + 'zuk1_2'] not in ['a','b','c']:
+        #    combi.loc[index, col] = 7
+        #    combi.loc[index, col + "Fall"] = 18
+        #if col == "GO" and row['ue_1'] == 0 and row[col + 'heu1_1'] in ['c'] and row[col + 'zuk1_1'] not in ['a','b','c'] and row[ col + 'zuk1_2']  in ['c']:
+        #    combi.loc[index, col] = 7
+        #    combi.loc[index, col + "Fall"] = 19
+        #if col == "GO" and row['ue_1'] == 0 and row[col + 'heu1_1'] not in ['a','b','c'] and row[col + 'zuk1_1'] in ['c'] and row[ col + 'zuk1_2'] not in ['a','b','c']:
+        #    combi.loc[index, col] = 7
+        #    combi.loc[index, col + "Fall"] = 20
+        #if col == "GO" and row['ue_1'] == 0 and row[col + 'heu1_1'] not in ['a','b','c'] and row[col + 'zuk1_1'] not in ['a','b','c'] and row[ col + 'zuk1_2'] in ['c']:
+        #    combi.loc[index, col] = 7
+        #    combi.loc[index, col + "Fall"] = 21
+
+
+
         #****************
         #Uebergang
             # Empfohlen 1
@@ -198,6 +203,20 @@ for col in treetypeslist:
                 combi.loc[index, col] = 6
                 combi.loc[index, col + "Fall"] = 21
 
+# Arve 6
+combi.loc[((combi["nais1"].isin(arvenundlaerchen)) & (combi["tahs"] == 'obersubalpin') & (
+            combi["hszukcor"] == 'hochmontan')), 'AR'] = 6
+combi.loc[((combi["nais1"].isin(arvenundlaerchen)) & (combi["tahs"] == 'obersubalpin') & (
+            combi["hszukcor"] == 'hochmontan')), 'AR'] = 6
+combi.loc[((combi["nais1"].isin(arvenundlaerchen)) & (combi["tahs"] == 'subalpin') & (
+            combi["hszukcor"] == 'hochmontan')), 'AR'] = 6
+combi.loc[((combi["nais2"].isin(arvenundlaerchen)) & (combi["tahsue"] == 'obersubalpin') & (
+            combi["hszukcor"] == 'hochmontan')), 'AR'] = 6
+combi.loc[((combi["nais2"].isin(arvenundlaerchen)) & (combi["tahsue"] == 'obersubalpin') & (
+            combi["hszukcor"] == 'hochmontan')), 'AR'] = 6
+combi.loc[((combi["nais2"].isin(arvenundlaerchen)) & (combi["tahsue"] == 'subalpin') & (
+            combi["hszukcor"] == 'hochmontan')), 'AR'] = 6
+
 combi.loc[(combi['hszukcor_2']==combi['hszukcor_1']),'naiszuk1_2']=combi['naiszuk1_1']
 combi.loc[(combi['hszukcor_2']==combi['hszukcor_1']),'naiszuk2_2']=combi['naiszuk2_1']
 
@@ -226,35 +245,3 @@ combi.columns
 combi.to_file(projectspace+"/GL"+"/GL_baumartenempfehlungen_combi.gpkg", layer="GL_baumartenempfehlungen_combi", driver="GPKG")
 print('all done')
 
-#unique pathways
-combi=gpd.overlay(baumartenbedeutungenrcp45, baumartenbedeutungenrcp85, how='intersection', make_valid=True, keep_geom_type=True)
-test45=baumartenbedeutungenrcp45[baumartenbedeutungenrcp45['nais']=='18M(48)']
-test85=baumartenbedeutungenrcp85[baumartenbedeutungenrcp85['nais']=='18M(48)']
-colist=combi.columns.tolist()
-combi=combi[['nais_1','tahs_1','tahsue_1','naiszuk1_1', 'naiszuk2_1','hszukcor_1','naiszuk1_2', 'naiszuk2_2','hszukcor_2','geometry']]
-combi.rename(columns={'nais_1':'nais_heute', 'tahs_1':'hs_heute','tahsue_1':'hsue_heute','naiszuk1_1':'nais1_rcp45', 'naiszuk2_1':'nais2_rcp45','hszukcor_1':'hs_rcp45','naiszuk1_2':'nais1_rcp85', 'naiszuk2_2':'nais2_rcp85','hszukcor_2':'hs_rcp85'}, inplace=True)
-combi.loc[(combi['hs_rcp85']==combi['hs_rcp45']),'nais1_rcp85']=combi['nais1_rcp45']
-combi.loc[(combi['hs_rcp85']==combi['hs_rcp45']),'nais2_rcp85']=combi['nais2_rcp45']
-combi['area']=combi.geometry.area
-#test = combi[combi['nais_heute']=='18M(48)']
-len(combi)
-combi.to_file(projectspace+"/GL"+"/GL_Projektionswege_combi.gpkg", layer="GL_Projektionswege_combi", driver="GPKG")
-combi=combi[combi['area']>=10000]
-combi.columns
-combi=combi[['nais_heute', 'hs_heute', 'hsue_heute', 'nais1_rcp45', 'nais2_rcp45','hs_rcp45', 'nais1_rcp85', 'nais2_rcp85', 'hs_rcp85']]
-#test=combi[((combi['hs_rcp85']==combi['hs_rcp45'])&(combi['nais2_rcp45']!=combi['nais2_rcp85']))]
-#testunique=test.drop_duplicates()
-#len(testunique)
-combi.columns
-combi_unique=combi.drop_duplicates()
-#combi_unique.loc[combi_unique['hs_heute']==10,'hs_heute']='obersubalpin'
-#combi_unique.loc[combi_unique['hs_heute']==9,'hs_heute']='subalpin'
-#combi_unique.loc[combi_unique['hs_heute']==8,'hs_heute']='hochmontan'
-#combi_unique.loc[combi_unique['hs_heute']==6,'hs_heute']='obermontan'
-#combi_unique.loc[combi_unique['hs_heute']==5,'hs_heute']='untermontan'
-#combi_unique.loc[combi_unique['hs_heute']==4,'hs_heute']='submontah'
-#combi_unique.loc[combi_unique['hs_heute']==2,'hs_heute']='collin'
-len(combi_unique)
-combi_unique=combi_unique[combi_unique['hs_heute'].isin(['obersubalpin','subalpin','hochmontan','obermontan','untermontan','submontan','collin'])]
-combi_unique=combi_unique.sort_values(by=['nais_heute'])
-combi_unique.to_excel(projectspace+'/GL/'+"/GL_Projektionspfade_unique.xlsx")
