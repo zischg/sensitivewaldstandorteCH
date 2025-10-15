3263732b6f5026bb2fb6490cb39091272be7eecc
Andreas Paul Zischg <andreas.zischg@unibe.ch>
2025-10-15 11:59:20 +0200

    Update CH_sensitiveBestaende_Fichte_TBk.py

--- CH_sensitiveBestaende_Fichte_TBk.py
+++ CH_sensitiveBestaende_Fichte_TBk.py
@@ -13,7 +13,7 @@ import warnings
 from osgeo import osr, gdal
 from sqlalchemy import create_engine
 mypassword=input("Enter database password: ").replace("'","")
-db_connection_url = "postgresql://azischg:mypassword@mobidb02.giub.unibe.ch:5432/ccwdb";
+db_connection_url = "postgresql://azischg:"+mypassword+"@mobidb02.giub.unibe.ch:5432/ccwdb";
 engine = create_engine(db_connection_url)
 #con=engine.connect()
 drv = gdal.GetDriverByName('GTiff')
@@ -30,6 +30,7 @@ warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
 #input data
 codeworkspace="C:/DATA/develops/sensitivewaldstandorteCH"
 projectspace="D:/CCW24sensi"
+#projectspace="C:/DATA"
 climatescenarios=['rcp45','rcp85']
 #climatescenario="rcp85"
 #climatescenario="rcp45"
@@ -65,20 +66,9 @@ threshold_stark_gef_min=40
 #threshold_mittel_gef_max=50
 #threshold_stark_gef_min=50
 
-
-#read Fichtenanteil
-#fi=gpd.read_file(projectspace+"/baumartenanteil.gpkg", layer='baumartenanteil')
-
-#read Bestandeskarte shape
-#bk_gdf=gpd.read_file(projectspace+"/TBk_CH/"+"TBk_Bestandeskarte.gpkg", layer='TBk_Bestandeskarte.gpkg')
-#bk_gdf.columns
-#bk_gdf=bk_gdf[['geometry']]
-#bk_gdf['FI']=0
-#bk_gdf['LH']=0
-
-#intersect baumartenanteil and Bestandeskarte is done in PostGIS and saved as gpkg
-#bk_gdf_fi=gpd.overlay(bk_gdf, fi, how='intersection)
-#bk_gdf_fi=gpd.read_file(projectspace+"/TBk_CH/"+"TBk_Bestandeskarte.gpkg", layer='TBk_Bestandeskarte.gpkg')
+#Arven, Laerchen und Foehren
+arvenundlaerchen=['59','59A','59C','59E','59J','59L','59S','59V','59H','59R','72,' '59*','59G','59AG','59EG','59VG','72G','57CLä','57VLä','58Lä', '59Lä', '59ELä', '59LLä', '59VLä','59LLä']
+foehren=['61','62','65','66','67','68','69','70','71','65*','66PM','67*','67G','68*','69G','70G','71G','72G']
 
 #******************************************************************************************************
 #Sensitive Bestaende
@@ -117,70 +107,58 @@ for climatescenario in climatescenarios:
     ##intersect Baumartenempfehlungen layer with Bestandeskarte layer
     print('intersect')
     #rcp_bk_gdf_in=gpd.overlay(rcp_baumartenempfehlungen_gdf_in, bk_gdf_fi, how='intersection', make_valid=True, keep_geom_type=True)
-    #rcp_bk_gdf_in=gpd.read_file(projectspace+"/"+"FR_"+climatescenario+"_baumartenempfehlungen_intsct_bk.gpkg", layer='FR_'+climatescenario+'_baumartenempfehlungen_intsct_bk')
-    sql='SELECT * FROM "'+climatescenario+'_TBk_Bestandeskarte_Baumartenanteil_Baumartenempfehlungen"'
-    rcp_bk_gdf_in=gpd.read_postgis(sql, con=engine, geom_col='geom')
-    rcp_bk_gdf_in["area"] = rcp_bk_gdf_in['geometry'].area
+    rcp_bk_gdf_in=gpd.read_file(projectspace+"/"+climatescenario+"_tbk_bestandeskarte_baumartenanteil_baumartenempfehlungen.gpkg", layer=climatescenario+'_tbk_bestandeskarte_baumartenanteil_baumartenempfehlungen')
+
+    #sql='SELECT * FROM "'+climatescenario+'_TBk_Bestandeskarte_Baumartenanteil_Baumartenempfehlungen"'
+    #rcp_bk_gdf_in=gpd.read_postgis(sql, con=engine, geom_col='geom')
+    #rcp_bk_gdf_in["area_m2"] = rcp_bk_gdf_in['geometry'].area
     mainlayercolumnslist=rcp_bk_gdf_in.columns.tolist()
     if 'index' in mainlayercolumnslist:
         rcp_bk_gdf_in.drop('index', axis=1, inplace=True)
     #rcp_bk_gdf_in.to_file(projectspace+"/FR/"+"FR_"+climatescenario+"_baumartenempfehlungen_intsct_bk.gpkg")
-    rcp_bk_gdf_out=rcp_bk_gdf_in.copy()
-    mainlayercolumnslist=rcp_bk_gdf_out.columns.tolist()
-    len(rcp_bk_gdf_out)
+    mainlayercolumnslist=rcp_bk_gdf_in.columns.tolist()
+    len(rcp_bk_gdf_in)
     #add columns to main layer
     #senstitive Bestaende pro Baumart:
     #-999 keine Angabe, 0 nicht klimasensitiv, 1 schwach klimasensitiv, 2 mittel klimasensitiv, 3 stark klimasensitiv, 4 bedingt klimasensitiv
     #Baumartenempfehlung:
     #-999 keine Angabe, 1 empfohlen, 2 bedingt empfohlen, 3 gefaehrdet, 4 in Zukunft empfohlen, 5 in Zukunft bedingt empfohlen
 
-    print("sb")
-    for col in treetypes:#_in_bk_list:
-        print(col)
-        #if col in mainlayercolumnslist:
-        rcp_bk_gdf_out["sb" + col] = -999
-        #nicht klimasensitiv - empfohlen
-        rcp_bk_gdf_out.loc[rcp_bk_gdf_out[((rcp_bk_gdf_out[col + '_2'] >= threshold_nicht_empfohlen_min) & (rcp_bk_gdf_out[col + '_2'] <= threshold_nicht_empfohlen_max) & (rcp_bk_gdf_out[col + '_1'].isin([1, 4])))].index, "sb" + col] = 0
-        #nicht klimasensitiv - bedingt empfohlen
-        rcp_bk_gdf_out.loc[rcp_bk_gdf_out[((rcp_bk_gdf_out[col + '_2'] >= threshold_nicht_bedingt_min) & (rcp_bk_gdf_out[col + '_2'] <= threshold_nicht_bedingt_max) & (rcp_bk_gdf_out[col + '_1'].isin([2, 5])))].index, "sb" + col] = 0
-        #nicht klimasensitiv - gefaehrdet
-        rcp_bk_gdf_out.loc[rcp_bk_gdf_out[((rcp_bk_gdf_out[col + '_2'] >= threshold_nicht_gef_min) & (rcp_bk_gdf_out[col + '_2'] <= threshold_nicht_gef_max) & (rcp_bk_gdf_out[col + '_1'].isin([3])))].index, "sb" + col] = 0
-        #schwach klimasensitiv - empfohlen
-        rcp_bk_gdf_out.loc[rcp_bk_gdf_out[((rcp_bk_gdf_out[col+'_2'] >threshold_schwach_empfohlen_min)&(rcp_bk_gdf_out[col+'_1'].isin([1,4])))].index, "sb"+col]=1
-        #schwach klimasensitiv - bedingt empfohlen
-        rcp_bk_gdf_out.loc[rcp_bk_gdf_out[((rcp_bk_gdf_out[col + '_2'] > threshold_schwach_bedingt_min) & (rcp_bk_gdf_out[col + '_2'] <= threshold_schwach_bedingt_max) & (rcp_bk_gdf_out[col + '_1'].isin([2, 5])))].index, "sb" + col] = 1
-        #schwach klimasensitiv - gefaehrdet
-        rcp_bk_gdf_out.loc[rcp_bk_gdf_out[((rcp_bk_gdf_out[col + '_2'] > threshold_schwach_gef_min) & (rcp_bk_gdf_out[col + '_2'] <= threshold_schwach_gef_max) & (rcp_bk_gdf_out[col + '_1'].isin([3])))].index, "sb" + col] = 1
-        #mittel klimasensitiv - bedingt empfohlen
-        rcp_bk_gdf_out.loc[rcp_bk_gdf_out[((rcp_bk_gdf_out[col + '_2'] > threshold_mittel_bedingt_min) & (rcp_bk_gdf_out[col + '_2'] <= threshold_mittel_bedingt_max) & (rcp_bk_gdf_out[col + '_1'].isin([2, 5])))].index, "sb" + col] = 2
-        #mittel klimasensitiv - gefaehrdet
-        rcp_bk_gdf_out.loc[rcp_bk_gdf_out[((rcp_bk_gdf_out[col + '_2'] > threshold_mittel_gef_min) & (rcp_bk_gdf_out[col + '_2'] <= threshold_mittel_gef_max) & (rcp_bk_gdf_out[col + '_1'].isin([3])))].index, "sb" + col] = 2
-        #stark klimasensitiv - bedingt empfohlen
-        rcp_bk_gdf_out.loc[rcp_bk_gdf_out[((rcp_bk_gdf_out[col+'_2'] > threshold_stark_bedingt_min) & (rcp_bk_gdf_out[col+'_1'].isin([2, 5])))].index, "sb" + col] = 3
-        # stark klimasensitiv - gefaehrdet
-        rcp_bk_gdf_out.loc[rcp_bk_gdf_out[((rcp_bk_gdf_out[col+'_2'] > threshold_stark_gef_min) & (rcp_bk_gdf_out[col+'_1'].isin([3])))].index, "sb" + col] = 3
-
-    #Arve
-    if 'AR_2' in mainlayercolumnslist:
-        rcp_bk_gdf_out.loc[rcp_bk_gdf_out[((rcp_bk_gdf_out[col+'_2']> 95) & (
-            rcp_bk_gdf_out[col+'_1'].isin([2, 5])))].index, "sb" + col] = 4
-        rcp_bk_gdf_out.loc[rcp_bk_gdf_out[((rcp_bk_gdf_out[col+'_2']> 40) & (
-            rcp_bk_gdf_out[col+'_1'].isin([3])))].index, "sb" + col] = 4
+    #calculate Fichtenanteil
+    rcp_bk_gdf_in['FIantTBk']=rcp_bk_gdf_in['FIantNHant']*rcp_bk_gdf_in['NH']
+    # Bei Arven und Laerchen wird der Fichtenanteil auf Null gesetzt
+    rcp_bk_gdf_in.loc[rcp_bk_gdf_in['nais1'].isin(arvenundlaerchen), 'FIantTBk'] = 0
+    # Bei Foehren und Laerchen wird der Fichtenanteil auf Null gesetzt
+    rcp_bk_gdf_in.loc[rcp_bk_gdf_in['nais1'].isin(foehren), 'FIantTBk'] = 0
+    print("sensitive bestaende FI")
+    rcp_bk_gdf_in["sbFI"] = -999
+    #nicht klimasensitiv - empfohlen
+    rcp_bk_gdf_in.loc[rcp_bk_gdf_in[((rcp_bk_gdf_in['FIantTBk'] >= threshold_nicht_empfohlen_min) & (rcp_bk_gdf_in['FIantTBk'] <= threshold_nicht_empfohlen_max) & (rcp_bk_gdf_in['FI'].isin([1, 4])))].index, "sbFI"] = 0
+    #nicht klimasensitiv - bedingt empfohlen
+    rcp_bk_gdf_in.loc[rcp_bk_gdf_in[((rcp_bk_gdf_in['FIantTBk'] >= threshold_nicht_bedingt_min) & (rcp_bk_gdf_in['FIantTBk'] <= threshold_nicht_bedingt_max) & (rcp_bk_gdf_in['FI'].isin([2, 5])))].index, "sbFI"] = 0
+    #nicht klimasensitiv - gefaehrdet
+    rcp_bk_gdf_in.loc[rcp_bk_gdf_in[((rcp_bk_gdf_in['FIantTBk'] >= threshold_nicht_gef_min) & (rcp_bk_gdf_in['FIantTBk'] <= threshold_nicht_gef_max) & (rcp_bk_gdf_in['FI'].isin([3])))].index, "sbFI"] = 0
+    #schwach klimasensitiv - empfohlen
+    rcp_bk_gdf_in.loc[rcp_bk_gdf_in[((rcp_bk_gdf_in['FIantTBk'] >threshold_schwach_empfohlen_min)&(rcp_bk_gdf_in['FI'].isin([1,4])))].index, "sbFI"]=1
+    #schwach klimasensitiv - bedingt empfohlen
+    rcp_bk_gdf_in.loc[rcp_bk_gdf_in[((rcp_bk_gdf_in['FIantTBk'] > threshold_schwach_bedingt_min) & (rcp_bk_gdf_in['FIantTBk'] <= threshold_schwach_bedingt_max) & (rcp_bk_gdf_in['FI'].isin([2, 5])))].index, "sbFI"] = 1
+    #schwach klimasensitiv - gefaehrdet
+    rcp_bk_gdf_in.loc[rcp_bk_gdf_in[((rcp_bk_gdf_in['FIantTBk'] > threshold_schwach_gef_min) & (rcp_bk_gdf_in['FIantTBk'] <= threshold_schwach_gef_max) & (rcp_bk_gdf_in['FI'].isin([3])))].index, "sbFI"] = 1
+    #mittel klimasensitiv - bedingt empfohlen
+    rcp_bk_gdf_in.loc[rcp_bk_gdf_in[((rcp_bk_gdf_in['FIantTBk'] > threshold_mittel_bedingt_min) & (rcp_bk_gdf_in['FIantTBk'] <= threshold_mittel_bedingt_max) & (rcp_bk_gdf_in['FI'].isin([2, 5])))].index, "sbFI"] = 2
+    #mittel klimasensitiv - gefaehrdet
+    rcp_bk_gdf_in.loc[rcp_bk_gdf_in[((rcp_bk_gdf_in['FIantTBk'] > threshold_mittel_gef_min) & (rcp_bk_gdf_in['FIantTBk'] <= threshold_mittel_gef_max) & (rcp_bk_gdf_in['FI'].isin([3])))].index, "sbFI"] = 2
+    #stark klimasensitiv - bedingt empfohlen
+    rcp_bk_gdf_in.loc[rcp_bk_gdf_in[((rcp_bk_gdf_in['FIantTBk'] > threshold_stark_bedingt_min) & (rcp_bk_gdf_in['FI'].isin([2, 5])))].index, "sbFI"] = 3
+    # stark klimasensitiv - gefaehrdet
+    rcp_bk_gdf_in.loc[rcp_bk_gdf_in[((rcp_bk_gdf_in['FIantTBk'] > threshold_stark_gef_min) & (rcp_bk_gdf_in['FI'].isin([3])))].index, "sbFI"] = 3
 
     #calculate maximum sensitivity over all treetypes
-    rcp_bk_gdf_out["maxsens"]=-999
-    rcp_bk_gdf_out["maxsens"]=rcp_bk_gdf_out[treetypes_sb].max(axis=1)
-    rcp_bk_gdf_out["area"] = rcp_bk_gdf_out["geometry"].area
-
-    #delete polygons where there are no FI
-    rcp_bk_gdf_out=rcp_bk_gdf_out[rcp_bk_gdf_out["Code"]>0]
-    #obersubalpin weglassen
-    rcp_bk_gdf_out = rcp_bk_gdf_out[rcp_bk_gdf_out["Code"] <10]
-    #Reine Waldfoehren - / Bergfoehrenstandorttypen weglassen.
+    rcp_bk_gdf_in["maxsens"]=rcp_bk_gdf_in["sbFI"]
 
     #write the output
     print('write output '+climatescenario)
-    rcp_bk_gdf_out.to_file(projectspace+"/FR/"+"FR_"+climatescenario+"_sensitivebestaende_FI_"+varname+".gpkg")
+    rcp_bk_gdf_in.to_file(projectspace+"/"+climatescenario+"_sensitivebestaende_FI_TBk_"+varname+".gpkg")
 
 print('all done')
 
