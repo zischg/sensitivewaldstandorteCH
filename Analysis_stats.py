210679d7d23d8a38a458b0119fb81d3c3174a2f0
Andreas Paul Zischg <andreas.zischg@unibe.ch>
2025-10-21 12:06:10 +0200

    Update Analysis_stats.py

--- Analysis_stats.py
+++ Analysis_stats.py
@@ -25,7 +25,18 @@ frequency = 2500  # Set Frequency To 2500 Hertz
 duration = 1000  # Set Duration To 1000 ms == 1 second
 winsound.Beep(frequency, duration)
 warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
+import matplotlib.pyplot as plt
 
+
+#color dictionaries
+# Define specific RGB colors for each class combination (normalized to [0,1])
+sensisto_colors_dict = {
+    0: (30.0/255.0,41.0/255.0,235.0/255.0),      # Kein klimasensitiver Standort, Dunkles Blau
+    2: (145.0/255.0,82.0/255.0,45.0/255.0),    # Klimasensitiver Standort, Braun
+    3: (239.0/255.0,180.0/255.0, 200.0/255.0),       # Langfristig klimasensitiver Standort, Rosarot
+    4: (248.0/255.0,238.0/255.0,51.0/255.0)}      # Teilweise klimasensitiver Standort, Gelb
+
+sensisto_labels = ['Kein klimasensitiver Standort', 'Klimasensitiver Standort', 'Langfristig klimasensitiver Standort', 'Teilweise klimasensitiver Standort']
 
 #input data
 codeworkspace="C:/DATA/develops/sensitivewaldstandorteCH"
@@ -37,10 +48,247 @@ climatescenarios=['rcp45','rcp85']
 #climatescenario="rcp26"
 
 #import data from database
+#Sensitive standorte
+#rcp45
+sql='SELECT * FROM public.rcp45_sensitivestandorte'
+sensisto_rcp45=gpd.read_postgis(sql, con=engine, geom_col='geom')
+sensisto_rcp45.columns
+storeg_list=sensisto_rcp45['storeg'].unique().tolist()
+storeg_list.sort()
+print(storeg_list)
+len(sensisto_rcp45)
+sensisto_rcp45=sensisto_rcp45[sensisto_rcp45['inanalysis']==1]
+len(sensisto_rcp45)
+tot_area=  sensisto_rcp45.geometry.area.sum()
+# Area statistics CH
+areastatistics_ch=sensisto_rcp45.groupby(['sensisto']).agg({'area_m2': 'sum'})
+areastatistics_ch['area_tot_pct']=areastatistics_ch['area_m2']/tot_area*100
+#areastatistics_ch['area_tot_ant']=areastatistics_ch['area_m2']/tot_area
+areastatistics_ch.to_excel(projectspace+'/areastatistics_rcp45_sensitivestandorte_CH.xlsx')
+#area statistics Standortregionen
+areastatistics=sensisto_rcp45.groupby(['storeg','sensisto']).agg({'area_m2': 'sum'})
+areastatistics['area_tot_pct']=areastatistics['area_m2']/tot_area*100
+areastatistics.to_excel(projectspace+'/areastatistics_rcp45_sensitivestandorte_regionen.xlsx')
+#create a pie chart for CH
+labels_zahlen = areastatistics.index.get_level_values('sensisto').tolist()
+print(labels_zahlen)
+print(sensisto_labels)
+sizes= areastatistics_ch['area_tot_pct'].tolist()
+
+
+# Create pie chart for CH RCP4.5 Sensitive Standorte
+fig, ax = plt.subplots(figsize=(8, 6))
+#ax.pie(sizes, labels=sensisto_labels, autopct='%1.1f%%', colors=[sensisto_colors_dict[key] for key in sensisto_colors_dict.keys()], startangle=90, textprops={'size': 'smaller'}, counterclock=False)
+# Create pie chart
+wedges, texts, autotexts = ax.pie(
+    sizes,
+    labels=None,  # Remove labels from pie slices
+    colors=[sensisto_colors_dict[key] for key in sensisto_colors_dict.keys()],
+    autopct='%1.1f%%',
+    startangle=90,
+    textprops={'fontsize': 10},
+    pctdistance=0.85,
+    counterclock=False)
+# Equal aspect ratio
+ax.axis('equal')
+# Set title
+ax.set_title('RCP 4.5 Sensitive Standorte CH', fontsize=12, fontweight='bold',pad=20)
+# CREATE CUSTOM LEGEND WITH LABELS
+legend_labels = sensisto_labels
+ax.legend(
+    wedges,
+    legend_labels,
+    #title="Sensitive Standorte",
+    loc="lower right",
+    #bbox_to_anchor=(1, 0, 0.5, 1),  # Position outside plot
+    bbox_to_anchor=(1.2, 0),
+    fontsize=9,
+    title_fontsize=12,
+    frameon=False,
+    fancybox=False,
+    shadow=False)
+plt.tight_layout()
+plt.savefig(projectspace+'/areastatistics_rcp45_sensitivestandorte_CH_piechart.png', dpi=300)
+plt.show()
+
+# Create pie chart for CH RCP4.5 Sensitive Standorte for each Standortregion
+for region in storeg_list:
+    print(region)
+    tot_area_region=  sensisto_rcp45[sensisto_rcp45['storeg']==region].geometry.area.sum()
+    areastatistics_region=sensisto_rcp45[sensisto_rcp45['storeg']==region].groupby(['sensisto']).agg({'area_m2': 'sum'})
+    areastatistics_region['area_tot_pct'] = areastatistics_region['area_m2'] / tot_area_region * 100
+    labels_zahlen = areastatistics.index.get_level_values('sensisto').tolist()
+    print(labels_zahlen)
+    print(sensisto_labels)
+    #sizes = areastatistics_region['area_tot_pct'].tolist()
+    sizes=[]
+    for key in labels_zahlen:
+        if key in areastatistics_region.index:
+            sizes.append(areastatistics_region.loc[key,'area_tot_pct'])
+        else:
+            sizes.append(0)
+    print(sizes)
+    fig, ax = plt.subplots(figsize=(8, 6))
+    #ax.pie(sizes, labels=sensisto_labels, autopct='%1.1f%%', colors=[sensisto_colors_dict[key] for key in sensisto_colors_dict.keys()], startangle=90, textprops={'size': 'smaller'}, counterclock=False)
+    # Create pie chart
+    wedges, texts, autotexts = ax.pie(
+        sizes,
+        labels=None,  # Remove labels from pie slices
+        colors=[sensisto_colors_dict[key] for key in sensisto_colors_dict.keys()],
+        autopct='%1.1f%%',
+        startangle=90,
+        textprops={'fontsize': 10},
+        pctdistance=0.85,
+        counterclock=False)
+    # Equal aspect ratio
+    ax.axis('equal')
+    # Set title
+    ax.set_title('RCP 4.5 Sensitive Standorte Region '+region, fontsize=12, fontweight='bold',pad=20)
+    # CREATE CUSTOM LEGEND WITH LABELS
+    legend_labels = sensisto_labels
+    ax.legend(
+        wedges,
+        legend_labels,
+        #title="Sensitive Standorte",
+        loc="lower right",
+        #bbox_to_anchor=(1, 0, 0.5, 1),  # Position outside plot
+        bbox_to_anchor=(1.2, 0),
+        fontsize=9,
+        title_fontsize=12,
+        frameon=False,
+        fancybox=False,
+        shadow=False)
+    plt.tight_layout()
+    plt.savefig(projectspace+'/areastatistics_rcp45_sensitivestandorte_piechart_'+region+'.png', dpi=300)
+    plt.show()
 
 
 
 
+
+
+
+
+#rcp85
+sql='SELECT * FROM public.rcp85_sensitivestandorte'
+sensisto_rcp85=gpd.read_postgis(sql, con=engine, geom_col='geom')
+sensisto_rcp85.columns
+storeg_list=sensisto_rcp85['storeg'].unique().tolist()
+storeg_list.sort()
+print(storeg_list)
+len(sensisto_rcp85)
+sensisto_rcp85=sensisto_rcp85[sensisto_rcp85['inanalysis']==1]
+len(sensisto_rcp85)
+tot_area=  sensisto_rcp85.geometry.area.sum()
+# Area statistics CH
+areastatistics_ch85=sensisto_rcp85.groupby(['sensisto']).agg({'area_m2': 'sum'})
+areastatistics_ch85['area_tot_pct']=areastatistics_ch85['area_m2']/tot_area*100
+#areastatistics_ch['area_tot_ant']=areastatistics_ch85['area_m2']/tot_area
+areastatistics_ch85.to_excel(projectspace+'/areastatistics_rcp85_sensitivestandorte_CH.xlsx')
+#area statistics Standortregionen
+areastatistics85=sensisto_rcp85.groupby(['storeg','sensisto']).agg({'area_m2': 'sum'})
+areastatistics85['area_tot_pct']=areastatistics85['area_m2']/tot_area*100
+areastatistics85.to_excel(projectspace+'/areastatistics_rcp85_sensitivestandorte_regionen.xlsx')
+#create a pie chart for CH
+labels_zahlen85 = areastatistics85.index.get_level_values('sensisto').tolist()
+print(labels_zahlen85)
+print(sensisto_labels)
+sizes85= areastatistics_ch85['area_tot_pct'].tolist()
+# Create pie chart for CH RCP8.5 Sensitive Standorte
+fig, ax = plt.subplots(figsize=(8, 6))
+#ax.pie(sizes, labels=sensisto_labels, autopct='%1.1f%%', colors=[sensisto_colors_dict[key] for key in sensisto_colors_dict.keys()], startangle=90, textprops={'size': 'smaller'}, counterclock=False)
+# Create pie chart
+wedges, texts, autotexts = ax.pie(
+    sizes85,
+    labels=None,  # Remove labels from pie slices
+    colors=[sensisto_colors_dict[key] for key in sensisto_colors_dict.keys()],
+    autopct='%1.1f%%',
+    startangle=90,
+    textprops={'fontsize': 10},
+    pctdistance=0.85,
+    counterclock=False)
+# Equal aspect ratio
+ax.axis('equal')
+# Set title
+ax.set_title('RCP 8.5 Sensitive Standorte CH', fontsize=12, fontweight='bold',pad=20)
+# CREATE CUSTOM LEGEND WITH LABELS
+legend_labels = sensisto_labels
+ax.legend(
+    wedges,
+    legend_labels,
+    #title="Sensitive Standorte",
+    loc="lower right",
+    #bbox_to_anchor=(1, 0, 0.5, 1),  # Position outside plot
+    bbox_to_anchor=(1.2, 0),
+    fontsize=9,
+    title_fontsize=12,
+    frameon=False,
+    fancybox=False,
+    shadow=False)
+plt.tight_layout()
+plt.savefig(projectspace+'/areastatistics_rcp85_sensitivestandorte_CH_piechart.png', dpi=300)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Create a 3x3 grid of pie charts
+fig, axes = plt.subplots(3, 3, figsize=(15, 15))
+# Plot each pie chart
+x=
+axes[0,0].pie([1], labels=sensisto_labels, colors=['lightgrey'], startangle=90)
+
+
+
+for i, (ax, dataset) in enumerate(zip(axes, data)):
+    # Extract labels and values
+    labels = list(dataset.keys())
+    sizes = list(dataset.values())
+
+    # Get colors for the current dataset
+    colors = [colors_dict[label] for label in labels]
+
+    # Create pie chart
+    ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
+
+    # Equal aspect ratio ensures pie is a circle
+    ax.axis('equal')
+
+    # Add title for each pie chart
+    ax.set_title(f'Combination {i + 1}', pad=20)
+
+# Create a separate legend for all class combinations
+handles = [plt.Rectangle((0, 0), 1, 1, color=colors_dict[label]) for label in colors_dict]
+plt.legend(handles, colors_dict.keys(), title="Class Combinations",
+           loc="center left", bbox_to_anchor=(1, 0.5), fontsize=10)
+
+# Adjust layout to prevent overlap
+plt.tight_layout()
+
+# Show plot
+plt.show()
+
+
+
+
+sql='SELECT * FROM public.rcp45_sensitivestandorte'
+sensisto_rcp85=gpd.read_postgis(sql, con=engine, geom_col='geom')
 
 
 
